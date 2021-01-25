0b40cdd0306996f638a7f6aa2629aadea6614b20
yujin mac <kimyu28jin@gmail.com>
2021-01-25 19:50:03 +0900

    context 위치 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./components/bookList";
 import SearchInput from "./components/searchInput";
-import { BookProvider } from "../bookContext";
+import { BookProvider } from "./bookContext";
 
 function App() {
   return (
