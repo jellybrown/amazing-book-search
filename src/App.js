a57d7f66b18aab44c47ffdd196d1988f2a847727
yujin mac <kimyu28jin@gmail.com>
2021-01-25 19:48:11 +0900

    app.js에 BookProvider 적용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./components/bookList";
 import SearchInput from "./components/searchInput";
+import { BookProvider } from "../bookContext";
 
 function App() {
   return (
-    <>
+    <BookProvider>
       <SearchInput />
       <BookList />
-    </>
+    </BookProvider>
   );
 }
 
