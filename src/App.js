89ad7105c71b642d38cd6143220c465b165d5c0f
yujin mac <kimyu28jin@gmail.com>
2021-01-25 19:23:28 +0900

    searchInput 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BookList from "./components/bookList";
+import SearchInput from "./components/searchInput";
 
 function App() {
   return (
     <>
+      <SearchInput />
       <BookList />
     </>
   );
--- src/components/searchInput.js
+++ src/components/searchInput.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchInput = () => {
-  return <div></div>;
+  const [term, setTerm] = useState("");
+  const onChange = (e) => {
+    setTerm(e.target.value);
+    console.log(term);
+  };
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    console.log(term);
+  };
+
+  return (
+    <>
+      <p>Book search</p>
+      <form onSubmit={onSearch}>
+        <input type="text" name="search" value={term} onChange={onChange} />
+        <button>검색</button>
+      </form>
+    </>
+  );
 };
 
 export default SearchInput;
