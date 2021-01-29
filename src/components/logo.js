4aae46c01c9ce37e3e77a1eefe08d1bbf9bcae40
yujin mac <kimyu28jin@gmail.com>
2021-01-29 18:59:28 +0900

    logo 컴포넌트 생성

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <span style={{ fontSize: "2rem", fontWeight: "bold" }}>Book.</span>
+      <span style={{ fontSize: "1rem" }}>search</span>
+    </div>
+  );
+};
+
+export default Logo;
